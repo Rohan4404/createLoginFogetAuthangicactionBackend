c546e2bfa08d1a9feed332f743888c2f5f649673
kumargaurav43 <gk156343@gmail.com>
2025-03-28 13:37:39 +0530

    fix forgot password api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -186,24 +186,7 @@ const forgotPassword = async (req, res) => {
 };
 
 // 🔄 Reset Password
-// const resetPassword = async (req, res) => {
-//   try {
-//     const { token } = req.params;
-//     const { newPassword } = req.body;
 
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findByPk(decoded.id);
-//     if (!user) return res.status(404).json({ error: "Invalid or expired token" });
-
-//     user.password = await bcrypt.hash(newPassword, 10);
-//     await user.save();
-//     res.status(200).json({ message: "Password has been reset successfully" });
-//   } catch (error) {
-
-//     console.log(error)
-//     res.status(500).json({ error: "Error resetting password", details: error.message });
-//   }
-// };
 const resetPassword = async (req, res) => {
   try {
     const { token } = req.params;
